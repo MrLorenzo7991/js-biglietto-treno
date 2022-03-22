22085a0b84c93409894f8d1673f1073720eefd77
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-22 17:44:08 +0100

    Corretto Pari o dispari

--- Bonus/PariDispari/main.js
+++ Bonus/PariDispari/main.js
@@ -1,10 +1,10 @@
 //Input Utente
     let giocataUtente = prompt("Scegli pari o dispari!");
-    let giocataUtenteConvertita = true;
+    let giocataUtenteConvertita = null;
     let numeroUtente = prompt("Scegli un numero compreso tra 1 e 9")
     giocataUtente = giocataUtente.toLowerCase();
     numeroUtente = parseInt(numeroUtente);
-    const numeroComputer = Math.floor(Math.random() * 10);
+    const numeroComputer = Math.floor(1+Math.random() * 11);
     let somma = 0;
 //Controllo input utente
     switch (giocataUtente) {
@@ -20,14 +20,14 @@
 //Calcoli
     if(numeroUtente >= 1 && numeroUtente <= 9){
         somma = numeroUtente + numeroComputer;
-        if(somma/2 == Math.floor(somma/2) && giocataUtente){
-            alert(somma +" "+"E' uscito pari, Hai Vinto!");
-        } else if (somma/2 == Math.floor(somma/2) && !giocataUtente){
-            alert(somma +" "+ "E' uscito dispari, Hai perso...");
-        } else if (somma/2 != Math.floor(somma/2) && !giocataUtente){
-            alert(somma +" "+ "E' uscito dispari, Hai Vinto!");
-        } else if (somma/2 != Math.floor(somma/2) && giocataUtente){
-            alert(somma +" "+ "E' uscito dispari, Hai perso...")
+        if(somma/2 == Math.floor(somma/2) && giocataUtenteConvertita){
+            alert("Hai scelto:" + " " + giocataUtente + "   " + "ed hai tirato:" + " " + numeroUtente + "   " + "Io ho tirato:" + " " + numeroComputer + "   " + "la somma è:" + somma + "   " + "E' uscito pari, Hai Vinto!");
+        } else if (somma/2 == Math.floor(somma/2) && !giocataUtenteConvertita){
+            alert("Hai scelto:" + " " + giocataUtente + "   " + "ed hai tirato:" + " " + numeroUtente + "   " + "Io ho tirato:" + " " + numeroComputer + "   " + "la somma è:" + somma + "   " + "E' uscito pari, Hai perso...");
+        } else if (somma/2 != Math.floor(somma/2) && !giocataUtenteConvertita){
+            alert("Hai scelto:" + " " + giocataUtente + "   " + "ed hai tirato:" + " " + numeroUtente + "   " + "Io ho tirato:" + " " + numeroComputer + "   " + "la somma è:" + somma + "   " + "E' uscito dispari, Hai Vinto!");
+        } else if (somma/2 != Math.floor(somma/2) && giocataUtenteConvertita){
+            alert("Hai scelto:" + " " + giocataUtente + "   " + "ed hai tirato:" + " " + numeroUtente + "   " + "Io ho tirato:" + " " + numeroComputer + "   " + "la somma è:" + somma + "   " + "E' uscito dispari, Hai perso...");
         }
     } else {
         alert("Scegli un numero compreso tra 1 e 9!");
