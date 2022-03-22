7ef614dbe596dee925f18c937a502150b96562d5
Lorenzo <lorenzo.miccio97@gmail.com>
2022-03-22 15:52:17 +0100

    Completato bonus età

---
+++ Bonus/eta.js
@@ -0,0 +1,11 @@
+//Input utente
+    etaUno = prompt("Inserisci l' età della prima persona");
+    etaDue = prompt("Inserisci l'eta della seconda persona");
+//Calcoli
+    if (etaUno > etaDue) {
+        alert ("La prima persona è più grande")
+    } else if (etaUno < etaDue) {
+        alert ("La seconda persona è più grande")
+    } else {
+        alert ("Le due persone hanno la stessa età")
+    }
